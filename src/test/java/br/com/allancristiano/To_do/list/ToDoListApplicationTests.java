8117491e539739aeb9579c5880d71d48079622e8
AllanCristiano <91546885+AllanCristiano@users.noreply.github.com>
2024-05-24 15:25:40 -0300

    first commit

---
+++ src/test/java/br/com/allancristiano/To_do/list/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.allancristiano.To_do.list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
